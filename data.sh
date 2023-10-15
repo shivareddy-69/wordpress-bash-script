b3d056bb39c0e75474830af569803c8587267e36
EC2 Default User <ec2-user@ip-172-30-1-118.ap-south-1.compute.internal>
2023-10-15 06:43:23 +0000

    for hosting

---
+++ data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo yum install -y git
+sudo yum install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
+sudo chmod 666 /var/run/docker.sock
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+git clone https://github.com/Ya-shu1/wordpress.git
+cd wordpress/
+docker-compose up -d
