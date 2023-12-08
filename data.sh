db29b27248ca942f9cd0022b5a594b6f2a6d8c4c
shivareddy-69 <126773087+shivareddy-69@users.noreply.github.com>
2023-12-08 13:05:35 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -7,5 +7,5 @@ sudo chmod 666 /var/run/docker.sock
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/shivareddy-69/docker-file.git
- cd docker-file/
+cd docker-file/
 docker-compose up -d
