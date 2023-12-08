6268724e4ab561291fa3a628f336c3b46690d3e0
shivareddy-69 <126773087+shivareddy-69@users.noreply.github.com>
2023-12-08 13:02:06 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,10 +3,9 @@ sudo yum install -y git
 sudo yum install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo usermod -aG docker ec2-user
 sudo chmod 666 /var/run/docker.sock
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/Ya-shu1/wordpress.git
-cd wordpress/
+git clone https://github.com/shivareddy-69/docker-file.git
+ cd docker-file/
 docker-compose up -d
